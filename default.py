9625abfe22270f89ca8244ccec1bb7925de9b217
domaemon <makoto.sugano@gmail.com>
2012-12-27 21:54:20 +0200

    prefix/postfix, null pointer

--- default.py
+++ default.py
@@ -4,25 +4,26 @@ import xbmc, xbmcgui
 woman = {}
 woman['to_dir'] = '/media/4b65836d-6b9a-47e9-b452-363f2e722f93/Koti'
 woman['from_dir'] = '/media/Nokia N9/DCIM/'
-woman['postfix'] = "_n"
+woman['prefix'] = 'n_'
 
 man = {}
 man['to_dir'] = '/media/4b65836d-6b9a-47e9-b452-363f2e722f93/Koti'
 man['from_dir'] = '/media/Nokia N9/DCIM'
-man['postfix'] = '_m'
+man['prefix'] = 'm_'
 
 profile_ptr = {}
 
-profiles = {'woman': woman, 'man': man}
-
 if ( __name__ == "__main__" ):
     dialog = xbmcgui.Dialog()
-    ret = dialog.select('Choose a profile', ['woman', 'man'])
+    ret = dialog.select('Choose a profile', ['Naoko', 'Makoto'])
 
     if (ret == 0):
         profile_ptr = woman;
     elif (ret == 1):
         profile_ptr = man;
+
+    if (not profile_ptr):
+        sys.exit();
     
     from_files = []
     from_files = os.listdir(profile_ptr['from_dir']);
@@ -38,15 +39,15 @@ if ( __name__ == "__main__" ):
         progress_percent = int(float(progress_numerator) / float(progress_denominator) * 100);
         dialog_progress.update(progress_percent, file)
         
-        m = re.match("((\d{6})\d{2}_\d{3}).(\w{3})", file);
+        m = re.match("(\d{2}\d{2})\d{4}.(\w{3})", file);
 
         if (m):
-            if (m.group(3) == "jpg"):
-                to_dir = os.path.join(profile_ptr['to_dir'], "MyPictures", m.group(2));
-            elif (m.group(3) == "mp4"):
-                to_dir = os.path.join(profile_ptr['to_dir'], "MyVideos", m.group(2));
+            if (m.group(2) == "jpg"):
+                to_dir = os.path.join(profile_ptr['to_dir'], "MyPictures", '20' + m.group(1));
+            elif (m.group(2) == "mp4"):
+                to_dir = os.path.join(profile_ptr['to_dir'], "MyVideos", '20' + m.group(1));
 
-            to_file = os.path.join(to_dir, m.group(1) + profile_ptr['postfix'] + "." + m.group(3));
+            to_file = os.path.join(to_dir, profile_ptr['prefix'] + m.group(0));
             from_dir = profile_ptr['from_dir']
             from_file = os.path.join(from_dir, file)
         
@@ -56,6 +57,7 @@ if ( __name__ == "__main__" ):
             
         # if the to_file does not exist..
             if (not os.path.isfile(to_file)):
+                print 'copying', from_file, ' to ', to_file;
                 shutil.copy(from_file, to_file);
 
 
